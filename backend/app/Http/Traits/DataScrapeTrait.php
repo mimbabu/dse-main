35c5c84eb88f295f7cee11a41e8ec3610a764fd6
mimbabu20 <mimbabu20@gmail.com>
2023-01-06 14:54:14 +0600

    env

--- backend/app/Http/Traits/DataScrapeTrait.php
+++ backend/app/Http/Traits/DataScrapeTrait.php
@@ -102,50 +102,122 @@ trait DataScrapeTrait
 
         // foreach ($companies as $com) {
         // print_r($com->code);
-
+        echo " $url\n";
 
         $page = $client->request('GET', $url);
         $companyName = $page->filter('.row .topBodyHead:first-child')->text();
         $companyName = substr($companyName, 14);
-        // Company Name
-        $company["name"] = $companyName;
 
 
+        $company = [];
 
-
-
-
-        $page->filter('#RightBody')->filter('tr')->each(function ($tr, $i) use (&$table) {
+        $page->filter('#company')->filter('tr')->each(function ($tr, $i) use (&$table) {
             $row = [];
             $tr->filter('td, th')->each(function ($td, $i) use (&$row) {
+                echo $td->text() . "<br/>";
                 array_push($row, $td->text());
             });
             array_push($table, $row);
         });
         // Market Capitalization (mn)
         // $company["market_capitalization_mn"] = str_replace(',', '', $table[7][3]);
-        // dd($url, $table);
-        $company["market_capital_mn"] = str_replace(',', '', $table[7][3]);
+        dd($url, $table);
+
+        $ltp = trim(str_replace('-', '', $table[1][1]));
+
+        $opening_price = trim(str_replace('-', '', $table[5][1]));
+
+        if (strlen($ltp) > 0 && strlen($opening_price) > 0) {
+            $company["name"] = $companyName;
+            $company["market_capital_mn"] = str_replace(',', '', $table[7][3]);
 
 
-        $company["authorized_capital_mn"] = str_replace(',', '', $table[8][1]);
+            $company["authorized_capital_mn"] = str_replace(',', '', $table[8][1]);
 
-        $company["paidup_capital_mn"] = str_replace(',', '', $table[9][1]);
+            $company["paidup_capital_mn"] = str_replace(',', '', $table[9][1]);
 
-        $company["total_outstanding_share_mn"] = str_replace(',', '', $table[11][1]);
+            $company["type_of_instrument"] = str_replace(',', '', $table[9][3]);
+
+            $company["face_par_value"] = str_replace(',', '', $table[10][1]);
+
+            $company["total_outstanding_share_mn"] = str_replace(',', '', $table[11][1]);
+
+            $company["cash_dividend"] = str_replace(',', '', $table[14][1]);
+            $company["bonus_issued_stock_dividend"] = str_replace(',', '', $table[15][1]);
+            if (!empty($table[39][6])) {
+                $company["current_pe"] = str_replace(',', '', $table[39][6]);
+            } else {
+                $company["current_pe"] = 0;
+            }
+
+            $company["sector"] = str_replace(',', '', $table[11][3]);
 
 
-        $company["category"] = $table[61][1];
 
-        $company["address"] = str_replace(',', '', $table[79][2]);
-        dd($table[81][1]);
-        $company["Contact Phone"] = str_replace(',', '', $table[81][1]);
-
-        $company["cell_no"] = str_replace(',', '', $table[86][1]);
-
+            $company["listing_since"] = $table[60][1];
+            $company["category"] = $table[61][1];
+            // dd($table[6][1]);
+            $company["address"] = str_replace(',', '', $table[79][2]);
+            $company["phone"] = str_replace(',', '', $table[81][1]);
 
 
-        $company["E-mail"] = $table[83][1];
+            $company["mobile_no"] = str_replace(',', '', $table[86][1]);
+
+            // dd($table[86][1]);
+
+            $company["email"] = $table[83][1];
+
+            $epsStart = 44;
+            $epsEnd = 0;
+            for ($i = 44; $i <= ($epsStart + 5); $i++) {
+                if ($table[$i] != null) {
+                    if ($table[$i][0] != "Year") {
+                        $company["eps"] = $table[$i][4];
+                        $company["nav"] = $table[$i][7];
+                        $epsEnd = $i;
+                    }
+                }
+            }
+            $dividendStart = ($epsEnd + 5);
+            $dividendEnd = 0;
+            for ($i = $dividendStart; $i <= ($dividendStart + 5); $i++) {
+                if ($table[$i] != null) {
+                    if ($table[$i][0] != "Details of Financial Statement") {
+                        $company["dividend"] = $table[$i][7];
+                        $company["dividend_yield"] = $table[$i][8];
+                        $dividendEnd = $i;
+                    }
+                }
+            }
+
+            $shareHoldingPatternStart = $dividendEnd + 6;
+
+
+            $currentYear = date('y');
+            $lastYear = $currentYear - 1;
+
+
+            while ($table[$shareHoldingPatternStart][0] != "Remarks") {
+
+
+                if (str_contains($table[$shareHoldingPatternStart][0], $currentYear)) {
+                    $company['sponsor_director'] = $table[$shareHoldingPatternStart][2];
+                    $company['govt'] = $table[$shareHoldingPatternStart][3];
+                    $company['institute'] = $table[$shareHoldingPatternStart][4];
+                    $company['foreign'] = $table[$shareHoldingPatternStart][5];
+                    $company['public'] = $table[$shareHoldingPatternStart][6];
+                    break;
+                }
+                if (str_contains($table[$shareHoldingPatternStart][0], $lastYear)) {
+                    $company['sponsor_director'] = $table[$shareHoldingPatternStart][2];
+                    $company['govt'] = $table[$shareHoldingPatternStart][3];
+                    $company['institute'] = $table[$shareHoldingPatternStart][4];
+                    $company['foreign'] = $table[$shareHoldingPatternStart][5];
+                    $company['public'] = $table[$shareHoldingPatternStart][6];
+                }
+                $shareHoldingPatternStart++;
+            }
+        }
         // dd($company);
 
         // $com->market_capital_mn = $company['market_capital_mn'];
--- backend/app/Http/Traits/StoreCompanySectorTrait.php
+++ backend/app/Http/Traits/StoreCompanySectorTrait.php
@@ -16,31 +16,62 @@ trait StoreCompanySectorTrait
      */
     public function storeCompanySector()
     {
-        $companies = Company::all();
+        $companies = Company::where('code', 'not like', 'TB%')->orderBy('code', 'asc')->get();
         foreach ($companies as $company) {
             if ($company->sector_id == null) {
                 $url = 'https://dsebd.org/displayCompany.php?name=' . $company->code;
                 $company_data = $this->scrapeCompany($url);
-                $company->name = $company_data['name'];
-                $company->market_capital_mn = $company_data['market_capital_mn'];
-                $company->authorized_capital_mn = $company_data['authorized_capital_mn'];
-                $company->paidup_capital_mn = $company_data['paidup_capital_mn'];
-                $company->total_outstanding_share_mn = $company_data['total_outstanding_share_mn'];
+                if (!empty($company_data['name'])) {
+                    $company->name = $company_data['name'];
 
-                $company->category = $company_data['category'];
+                    $company->market_capital_mn = $this->filter_data($company_data['market_capital_mn']);
 
+                    $company->authorized_capital_mn = $this->filter_data($company_data['authorized_capital_mn']);
 
 
+                    $company->paidup_capital_mn = $this->filter_data($company_data['paidup_capital_mn']);
+                    $company->type_of_instrument = $company_data['type_of_instrument'];
+                    $company->face_par_value = $company_data['face_par_value'];
+                    $company->total_outstanding_share_mn = $this->filter_data($company_data['total_outstanding_share_mn']);
+                    $company->cash_dividend = $company_data['cash_dividend'];
+                    $company->bonus_issued_stock_dividend = $company_data['bonus_issued_stock_dividend'];
+                    $company->current_pe = $company_data['current_pe'];
+                    // $company->sector = $company_data['sector'];
+                    $company->listing_since = $company_data['listing_since'];
+                    $company->category = $company_data['category'];
+                    $company->address = $company_data['address'];
+                    $company->phone = $company_data['phone'];
+                    $company->mobile_no = $company_data['mobile_no'];
+                    $company->eps = $company_data['eps'];
+                    $company->nav = $company_data['nav'];
+                    $company->dividend = $company_data['dividend'];
+                    $company->dividend_yield = $company_data['dividend_yield'];
+                    $company->sponsor_director = $company_data['sponsor_director'];
+                    $company->govt = $company_data['govt'];
+                    $company->institute = $company_data['institute'];
+                    $company->foreign = $company_data['foreign'];
+                    $company->public = $company_data['public'];
+                    $company->email = $company_data['email'];
 
-                $sector = Sector::where('name', $company_data[2])->first();
-                if (!$sector) {
-                    $sector = new Sector;
-                    $sector->name = $company_data[2];
-                    $sector->save();
+
+
+
+                    $sector = Sector::where('name', $company_data['sector'])->first();
+                    if (!$sector) {
+                        $sector = new Sector;
+                        $sector->name = $company_data['sector'];
+                        $sector->save();
+                    }
+                    $company->sector_id = $sector->id;
+                    $company->save();
                 }
-                $company->sector_id = $sector->id;
-                $company->save();
             }
         }
+    }
+
+    function filter_data($input)
+    {
+        $fiterData = trim(str_replace('-', '', $input));
+        return (strlen($fiterData) > 0) ? $fiterData : 0;
     }
 }
